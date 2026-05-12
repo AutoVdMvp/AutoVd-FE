14d10035ce6ba13aee4620fa5e481a0a757ffcc1
josephpark <rkfnahs12e@gmail.com>
2026-05-12 22:12:53 +0900

    style : 슬라이드 및 메인화면 디자인 초기 구현

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -11,11 +11,39 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24">
-      <h1 className="text-xl font-bold">AutoVD</h1>
-      <div className="flex flex-col m-3.5 gap-3">
-        <div className="relative">
-          <ArticleEditor onSubmit={handleSubmit} />
+    <main className="relative flex min-h-screen">
+      <div className="flex flex-col min-h-full gap-8 py-4 pl-4 bg-blue-500 w-[288px]">
+        <div className="flex items-center justify-between gap-3">
+          <div className="text-xl font-bold text-amber-300">Auto VD</div>
+          <div>슬라이드바 이모티콘</div>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <div className="flex gap-3">
+            <div>아이콘</div>
+            <span>내 정보</span>
+          </div>
+          <div className="flex gap-3">
+            <div>아이콘</div>
+            <span>내 정보</span>
+          </div>
+          <div className="flex gap-3">
+            <div>아이콘</div>
+            <span>내 정보</span>
+          </div>
+
+          <div className="flex gap-3">
+            <div>아이콘</div>
+            <span>내 정보</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center flex-1">
+        <h1 className="text-xl font-bold">AutoVD</h1>
+        <div className="flex flex-col m-3.5 gap-3">
+          <div className="relative">
+            <ArticleEditor onSubmit={handleSubmit} />
+          </div>
         </div>
       </div>
     </main>
